7d5331173cbf3ddcbdd2077ade25e28ec7419382
Aleksandar Jovanovic <877salle@gmail.com>
2019-10-21 11:09:48 +0200

    header modified width

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Row, Col, } from 'reactstrap'
+import { Row, Col, Container } from 'reactstrap'
 import Logo from '../assets/images/logo WEB.png'
 import NavigationMeny from './Nav'
 import { Link } from 'react-router-dom'
@@ -8,18 +8,20 @@ class Header extends Component {
     render() {
         return (
             <div>
-                <Row id="HeaderMeny">
-                    <Col lg="2" md="3" xs="4">
-                        <Link to="/">
-                            <div>
-                                <img src={Logo} alt="bella-dent" width="100%"></img>
-                            </div>
-                        </Link>
-                    </Col>
-                    <Col className="align-self-center">
-                        <NavigationMeny />
-                    </Col>
-                </Row>
+                <Container>
+                    <Row id="HeaderMeny">
+                        <Col lg="2" md="3" xs="4">
+                            <Link to="/">
+                                <div>
+                                    <img src={Logo} alt="bella-dent" width="100%"></img>
+                                </div>
+                            </Link>
+                        </Col>
+                        <Col className="align-self-center">
+                            <NavigationMeny />
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         )
     }
