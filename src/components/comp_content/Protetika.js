f0b5b2a25e9940815d0e6c650142d6c80d4338e8
Aleksandar Jovanovic <877salle@gmail.com>
2019-10-21 10:24:10 +0200

    update-1

--- src/components/comp_content/Protetika.js
+++ src/components/comp_content/Protetika.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 import { Container, Row, Col } from 'reactstrap'
 import Slider from '../Carusel'
-
+import pr1 from '../../assets/images/protetika1.jpg'
+import pr2 from '../../assets/images/protetika21.jpg'
+import pr3 from '../../assets/images/protetika22.jpg'
 
 class Protetika extends Component {
     render() {
@@ -9,10 +11,7 @@ class Protetika extends Component {
             <div>
                 <Slider />
                 <Container>
-                    <Row>
-                        <Col md="5">
-                            SLIKE
-                        </Col>
+                    <Row className="p-3">
                         <Col>
                             <h3 className='textCenter'>Protetika</h3>
                             <h4 className='textCenter'>Proteze (parcijalne i totalne)</h4>
@@ -21,6 +20,14 @@ class Protetika extends Component {
                         <br />
                                 Parcijalna skeletirana (vizil)  proteza je kvalitetnije rešenje jer osim veće izdržljivosti zbog metalnog skeleta, pacijentu pruža i veći komfor zbog manje površine dela proteze koji se naslanja na meka tkiva ( nepce, podjezični predeo). Na taj način je smanjen osećaj stranog tela u ustima. Sve proteze se izrađuju u vrlo kratkom roku.
                         </p>
+                        </Col>
+                        <Col md="5">
+                            <img src={pr1} width='100%' />
+                        </Col>
+                    </Row>
+
+                    <Row className="p-3">
+                        <Col>
                             <h4 className='textCenter'>FIKSNA PROTETIKA</h4>
                             <p className='textCenter'>
                                 Fiksna protetika je proces koji predstavljaju nadoknade koje se trajno cementiraju na postojeće zube. Nadoknađuju destrukcije prirodne krune zuba, nastale usled velikih karijesnih lezija i trauma. Mogu poslužiti i kao estetsko preoblikovanje postojećih zuba ukoliko pacijenti nisu zadovoljni sa bojom, oblikom, veličinom ili položajem.
@@ -33,6 +40,10 @@ class Protetika extends Component {
                             <p className='textCenter'>
                                 Cirkonijum-dioksid i Litijum-disilikat su materijali koji su poslednjih nekoliko godina napravili istinsku revoluciju u stomatologiji. Osnovne karakteristike ovih materijala su prelamanje svetlosti i transparencija koja je vrlo slična prirodnom zubu. Pored ovoga, ove materijale karakteriše i izuzetna mehanička otpornost koja je na nivou ranije korištenog metala. Dok se istovremeno postiže odličan estetski efekat zahvaljujući keramici na površini. Osim visoke izdržljivosti i odlične estetike, ove nadoknade su i finansijski povoljne pa predstavljaju i najčešći izbor pacijenata.
                         </p>
+                        </Col>
+                        <Col md="5" className="textCenter">
+                            <img className="p-3" src={pr2} width='80%' />
+                            <img className="p-3" src={pr3} width='80%' />
                         </Col>
                     </Row>
                 </Container>
