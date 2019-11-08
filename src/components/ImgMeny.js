f230a5925ea97f6b9ff78f5b6c72a6dc02d6a903
Aleksandar Jovanovic <877salle@gmail.com>
2019-11-08 09:37:34 +0100

    modified breakingpoint

--- src/components/ImgMeny.js
+++ src/components/ImgMeny.js
@@ -18,7 +18,7 @@ class ImgMeny extends Component {
                 new WOW.WOW().init();
             }
         }
-        var x = window.matchMedia("(max-width: 700px)")
+        var x = window.matchMedia("(max-width: 800px)")
         myFunction(x) // Call listener function at run time
         x.addListener(myFunction) // Attach listener function on state changes
 
