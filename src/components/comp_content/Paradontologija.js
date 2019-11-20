31ed4c8d71c9af74e8724ad08cf044087a19b48a
Aleksandar Jovanovic <877salle@gmail.com>
2019-11-20 11:07:37 +0100

    ,

--- src/components/comp_content/Paradontologija.js
+++ src/components/comp_content/Paradontologija.js
@@ -12,17 +12,17 @@ class Paradontologija extends Component {
                         <Col>
                             <h3 className='textCenter title'>Paradontologija</h3>
                             <h4 className='textCenter title'>Usluge koje pružamo iz ove oblasti:</h4>
-                            <p className='textCenter'>
+                            <p>
                                 - Uklanjanje kamenca <br />
                                 - Parodontalne operacije <br />
                                 - Uklanjanje mekih naslaga <br />
                                 - Terapija parodontalnih džepova
                             </p>
                             <h4 className='textCenter title'>Paradontopatija</h4>
-                            <p className='textCenter'>
+                            <p>
                                 Parodontopatija je hronično oboljenje desni u uznapredovaloj fazi. Paradentoza je oboljenje potpornog aparata zuba (parodoncijuma), koje, ukoliko se ne leči, u svom poslednjem stadijumu dovodi do vidljivog povlačenja gingive, rasklaćenja i ispadanja zuba.
                         </p>
-                            <p className='textCenter'>
+                            <p>
                                 Oboljenje počinje čestim zapaljenjem desni i krvarenjem tokom pranja zuba. Zatim dolazi do povlačenja desni i ogolićenja vratova zuba, te zubi izgledaju duži. Između zuba nastaju parodontalni džepovi u koje zapada hrana, stanje se pogoršava, pa vremenom dolazi do rasklaćenja zuba i njihovog gubitka. Da bi se sve ovo sprečilo, potrebno je dobro održavati oralnu higijenu i lečiti oboljenje kada je još u početnom stadijumu. Lečenje kao prvo podrazumeva specijalno čišćenje zuba i njihovog potpornog aparata, primenu različitih medikamenata (sistemski i lokalno), a nekada je neophodna i hirurška intervencija/terapija.Oboljenje desni može trajati godinama, bez bolova i neprimetno, što je nažalost, veoma često, razlog kasnog odlaska kod stomatologa, tj. u uznapredovaloj fazi bolesti.
                         </p>
                         </Col>
